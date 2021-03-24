c6335d2d299b3f59958ac967bf0da3ee8634e330
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-03-24 15:45:58 +0530

    final

--- js/index.js
+++ js/index.js
@@ -2,20 +2,39 @@ var i = 1;
 
 
 function add() {
+    var error = 0;
+    nam = document.getElementById("name").value;
+    num = document.getElementById("num").value;
+    email = document.getElementById("email").value;
 
-    if (document.getElementById("name").value && document.getElementById("num").value && document.getElementById("email").value) {
+    
+    if(nam=="" || num=="" || email==""){
+        alert("No field should be blank");
+        error = 1;
+    }
+    if (nam.length < 3) {
+        error = 1;
+    }
+    if (num.length < 10) {
+        error = 1;
+    }
+    console.log(error)
+    var atposition = email.indexOf("@");
+    var dotposition = email.lastIndexOf(".");
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= email.length) {
+        error = 1;
+    }
+
+    if (error === 0) {
 
         document.getElementById("info").style.display = "flex";
 
-        name = document.getElementById("name").value;
-        num = document.getElementById("num").value;
-        email = document.getElementById("email").value;
 
         var table = document.getElementById("info");
         var trow = table.insertRow(i);
 
-        var tname = trow.insertCell(0);
-        tname.innerText = name;
+        var tnam = trow.insertCell(0);
+        tnam.innerText = nam;
 
         var tmob = trow.insertCell(1);
         tmob.innerText = num;
@@ -23,13 +42,13 @@ function add() {
         var tmail = trow.insertCell(2);
         tmail.innerText = email;
 
-        var delbutton= trow.insertCell(3);
+        var delbutton = trow.insertCell(3);
         delbutton.innerHTML = '<input type="button" id="delete" value="Delete" onclick="deleteRow(this)">';
 
         i++;
     }
     else {
-        alert("One or many field/s are empty");
+        alert("Input is not correct");
     }
 }
 
@@ -37,4 +56,4 @@ function deleteRow(r) {
     var x = r.parentNode.parentNode.rowIndex;
     document.getElementById("info").deleteRow(x);
     i--;
-  }
+}
